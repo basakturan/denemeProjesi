1ed9ab160823cda734ab893a7201c8112f430fe3
basakturan <basakturan278@gmail.com>
2018-11-19 11:08:25 +0300

    guncelleme yapıldı

--- src/main/java/testClass.java
+++ src/main/java/testClass.java
@@ -3,6 +3,7 @@ public class testClass {
     @Override
     public String toString() {
 
-        System.out.println("Bu bizim ilk github denememiz");
+        System.out.println("ilk projemizde güncelleme yaptık...");
+        System.out.println("bunu başak eklememi istedi");
     }
 }
