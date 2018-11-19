ec6b1f48fbe42ac050c69dc89a203d23a2cfe925
basakturan <basakturan278@gmail.com>
2018-11-19 11:21:20 +0300

    klasor yeniden eklendi

---
+++ baskaBirProje/src/main/java/testClass.java
@@ -0,0 +1,9 @@
+public class testClass {
+
+    @Override
+    public String toString() {
+
+        System.out.println("ilk projemizde güncelleme yaptık...");
+        System.out.println("bunu başak eklememi istedi");
+    }
+}
